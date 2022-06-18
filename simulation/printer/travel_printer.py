ce1326d6002c22c835a1f211ecbdb0c194dc1a1c
heralex98 <hernanigomes15@gmail.com>
2022-06-18 19:16:42 -0400

    Pasajeros en el CSV

--- simulation/printer/travel_printer.py
+++ simulation/printer/travel_printer.py
@@ -2,7 +2,7 @@ from routes.travel import Travel
 
 TravelsCSV = 'CSV/' + 'TravelsCSV.csv'
 prueba = open('CSV/' + 'TravelsCSV.csv', 'w')
-titulador = 'UnitId,Lat,Lon,DistR,OPedId,TPedId,Dis,ETA,RouteOwner,RouteId\n'
+titulador = 'UnitId,Lat,Lon,DistR,OPedId,TPedId,Dis,ETA,RouteOwner,RouteId,Pasajeros\n'
 prueba.write(titulador)
 
 
@@ -27,5 +27,5 @@ class TravelPrinter:
                           travel.unit.current_position.longitude, travel.unit.mileage,
                           travel.origin_ped.uid, travel.target_ped.uid,
                           travel.distance, travel.time_to_arrival * 60,  travel.route.owner_id, travel.route.uid))
-            lista = str('{:3d}'.format(travel.unit.uid)) + ',' + str('{:10.8f}'.format(travel.unit.current_position.latitude)) + ',' + str('{:10.8f}'.format(travel.unit.current_position.longitude)) + ',' + str('{:12.8f}'.format(travel.unit.mileage)) + ',' + str('{:3d}'.format(travel.origin_ped.uid)) + ',' + str('{:3d}'.format(travel.target_ped.uid)) + ',' + str('{:10.8f}'.format(travel.distance)) + ',' + str('{:10.8f}'.format(travel.time_to_arrival * 60)) + ',' + str('{:3d}'.format(travel.route.owner_id)) + ',' + str('{:3d}'.format(travel.route.uid))+'\n'
+            lista = str('{:3d}'.format(travel.unit.uid)) + ',' + str('{:10.8f}'.format(travel.unit.current_position.latitude)) + ',' + str('{:10.8f}'.format(travel.unit.current_position.longitude)) + ',' + str('{:12.8f}'.format(travel.unit.mileage)) + ',' + str('{:3d}'.format(travel.origin_ped.uid)) + ',' + str('{:3d}'.format(travel.target_ped.uid)) + ',' + str('{:10.8f}'.format(travel.distance)) + ',' + str('{:10.8f}'.format(travel.time_to_arrival * 60)) + ',' + str('{:3d}'.format(travel.route.owner_id)) + ',' + str('{:3d}'.format(travel.route.uid))+ ',' + str('{:3d}'.format(len(travel.unit.passengers)))+'\n'
             prueba.write(lista)
